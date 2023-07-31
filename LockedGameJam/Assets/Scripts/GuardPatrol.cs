96b2486e3071309af400acb1b95a2d5c7f47331c
Laia <laia.fustercorral@gmail.com>
2023-07-31 14:46:36 +0200

    Added patrol lines (maybe broke some guards' patrol direction and distance)

--- LockedGameJam/Assets/Scripts/GuardPatrol.cs
+++ LockedGameJam/Assets/Scripts/GuardPatrol.cs
@@ -8,11 +8,32 @@ public class GuardPatrol : MonoBehaviour
     [SerializeField] float speed = 2;
     [SerializeField] float maxDistance = 2;
 
+    [Header("Patrol Line Config")]
+    [SerializeField] Material patrolLineMaterial;
+    [SerializeField] float patrolLineWidth = 0.1f;
+    [SerializeField] Color patrolLineColor;
+
     Vector2 startPosition;
 
     void Start()
     {
         startPosition = transform.position;
+        PaintPatrolLine();
+    }
+
+    private void PaintPatrolLine()
+    {
+        GameObject patrolLine = new GameObject("patrolLine");
+        LineRenderer line = patrolLine.AddComponent<LineRenderer>();
+        line.startColor = patrolLineColor;
+        line.endColor = patrolLineColor;
+        line.startWidth = patrolLineWidth;
+        line.endWidth = patrolLineWidth;
+        line.SetPosition(0, startPosition + direction * maxDistance);
+        line.SetPosition(1, startPosition - direction * maxDistance);
+        line.material = patrolLineMaterial;
+        line.numCapVertices = 3;
+        patrolLine.transform.SetParent(transform);
     }
 
     // Update is called once per frame
